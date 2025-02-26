f46e23ad68038ffc40e712598d1230fac3d542ac
rellagant <barbara.cs@gmail.com>
2025-02-26 13:07:12 -0500

    added footer to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import "./Home.scss";
 import Header from "../../components/Header/Header";
 import InfoCard from "../../components/InfoCard/InfoCard";
 import ArticleList from "../../components/ArticleList/ArticleList";
+import Footer from "../../components/Footer/Footer";
+
 export function Home() {
     return (
         <div className="home-container">
             <Header />
             <InfoCard />
             <ArticleList />
+            <Footer />
         </div>
     )
 }
