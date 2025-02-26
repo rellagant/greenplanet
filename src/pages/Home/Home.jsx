0df120f6af7fe3fb1a7abbe5b80ff341fc6412ae
rellagant <barbara.cs@gmail.com>
2025-02-26 03:29:44 -0500

    minor routing updates; errant code cleanup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import "./Home.scss";
 import Header from "../../components/Header/Header";
 import InfoCard from "../../components/InfoCard/InfoCard";
-
+import ArticleList from "../../components/ArticleList/ArticleList";
 export function Home() {
     return (
         <div className="home-container">
             <Header />
             <InfoCard />
+            <ArticleList />
         </div>
     )
 }
